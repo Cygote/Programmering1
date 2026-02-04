5dcd8fce69172ff59787c46f482b284628e716f9
cygote <cygote123@gmail.com>
2026-02-04 11:40:44 +0100

    Func training

---
+++ Forfun/AreaCirkel/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿// TRÄNING FUNKTIONER //
+// DEL 1 - Area//
+
+/*static int calculate_square_area(int sida)
+{
+	return sida * sida;
+}
+
+int result = calculate_square_area(7);
+Console.WriteLine($"Arean är: {result}");
+*/
+
+//**********************
+// DEL 2 - Räkna vokaler
+/*
+class Program
+{
+	static int RäknaVokaler(string text)
+	{
+		int antalVokaler = 0;
+
+		foreach (char bokstav in text)
+		{
+			char litenBokstav = char.ToLower(bokstav);
+
+			if ("aeiouyåäö".Contains(litenBokstav))
+			{
+				antalVokaler++;
+			}
+		}
+
+		return antalVokaler;
+	}
+
+	static void Main()
+	{
+		string text = "Programmering är roligt!";
+		int resultat = RäknaVokaler(text);
+
+		Console.WriteLine($"Antal vokaler: {resultat}");
+	}
+}
+*/
+
+//************************
+// Del 3 - 
+
+
+
+
+
+
