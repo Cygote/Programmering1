d8ca77308f598ba1b6e55a588fd1f6f1ea5d3d03
cygote <cygote123@gmail.com>
2026-02-05 14:00:42 +0100

    OvertimeCalc

---
+++ Training5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿double CalculateSalary(double hoursWorked,
+double hourlyRate)
+{
+	double regularHours = Math.Min(hoursWorked, 40);
+	double overtimeHours = Math.Max(0, hoursWorked - 40);
+	double regularPay = regularHours * hourlyRate;
+	double overtimePay = overtimeHours * hourlyRate * 1.5;
+	return regularPay + overtimePay;
+}
+
+double hours = 45;
+double rate = 200;
+double totalSalary = CalculateSalary(hours,rate);
+
+Console.WriteLine($"Total lön:{totalSalary}kr");
